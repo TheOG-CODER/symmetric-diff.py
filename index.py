c19df28506615c0ae6a487331c33cca475ef77d8
TheOG-CODER <jeswinaaron2005@gmail.com>
2024-10-25 14:25:22 +0530

    Add files via upload

--- index.py
+++ index.py
@@ -1,18 +1,8 @@
-#To find the symmetric difference of two sets
-M=int(input("set1 : "))
-a=set()
-for i in range(0,M):
-  x=int(input(end=""))
-  a.add(x)
-
-N=int(input("set2 : "))
-b=set()
-for i in range(0,M):
-  y=int(input(end=""))
-  b.add(y)
-
-z=set()
-z=((a|b)-(a&b))
-
-for i in z:
-    print(i)
+arr=[]
+n = int(input("Enter the number of elemnts : "))
+for i in range(0,n):
+    x=int(input())
+    arr.append(x)
+print(arr)
+z=set(arr)
+print(z)
